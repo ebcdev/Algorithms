375119c371464c1340bf6457cfa0fb973d93a8d0
Enrique Berumen <enriqueebc@gmail.com>
2022-02-17 00:44:39 -0600

    Max Heap Implementation

--- heap/Program.cs
+++ heap/Program.cs
@@ -1,22 +1,50 @@
-﻿
+﻿Heap heap=new Heap();
+var values=new int [] {4,1,3,2,16,9,10,14,8,7};
+var maxHeap=heap.BuildMaxHeap(values);
+
+values.ToList().ForEach(a=>Console.Write($"{a}-"));
+maxHeap.ToList().ForEach(a=>Console.Write($"{a}-"));
 
 
 public class Heap{
 
-
+ 
     
 
     public int[] BuildMaxHeap(int [] heap){
         int heapSize=heap.Length;
-        for(int i=heapSize/2;i>0;i--){
-
+        for(int i=heapSize/2-1;i>=0;i--){
+            heap=MaxHeapify(heap,i);
         }
         return heap;
     }
 
 
     public int [] MaxHeapify(int [] A,int i){
-        int 
+        int left =i.Left();
+        int right=i.Right();
+
+        int largest =0;
+       
+
+        if(left<A.Length && A[left]>A[i]){
+            largest=left;
+        }else{
+            largest=i;
+        }
+
+        if(right<A.Length && A[right]>A[largest]){
+            largest=right;
+        }
+
+        if(largest!=i){
+            var temp =A[i];
+            A[i]=A[largest];
+            A[largest]=temp;
+            return MaxHeapify(A,largest);
+        }
+        return A;
+        
     }
 
 
@@ -30,14 +58,14 @@ public class Heap{
 
 public static class Extensions{
     public static int Parent(this int i){
-        return i/2;
+        return (i+1)/2-1;
     }
 
     public static int Left(this int i){
-        return 2*i;
+        return 2*(i+1)-1;
     }
 
     public static int Right(this int i){
-        return 2*i+1;
+        return 2*(i+1);
     }
 }
